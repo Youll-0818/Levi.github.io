2bf64b3db3cddbdfe5b2f6434ff69188601799d1
Youll-0818 <levi.you@foxmail.com>
2024-11-09 11:43:56 +0800

    Site updated: 2024-11-09 11:43:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/08/hello-world/"];function toRandomPost(){
+var posts=["2024/11/08/hello-world/","2024/11/09/浏览器搜索丨高效搜索/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
